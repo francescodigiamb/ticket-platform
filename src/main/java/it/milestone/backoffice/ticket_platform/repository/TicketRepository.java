1621e98f6223bd5d29aeb6f32490e3d570705f78
francescodigiamb <francescodigiamb@gmail.com>
2024-12-15 19:12:30 +0100

    API

--- src/main/java/it/milestone/backoffice/ticket_platform/repository/TicketRepository.java
+++ src/main/java/it/milestone/backoffice/ticket_platform/repository/TicketRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import it.milestone.backoffice.ticket_platform.model.State;
 import it.milestone.backoffice.ticket_platform.model.Ticket;
 import it.milestone.backoffice.ticket_platform.model.User;
 
@@ -16,5 +17,9 @@ public interface TicketRepository extends JpaRepository<Ticket, Integer> {
     List<Ticket> findByOperator(User operator);
 
     List<Ticket> findByOperatorUsernameAndTitleContainingIgnoreCase(String username, String text);
+
+    List<Ticket> findByCategory_Name(String name);
+
+    List<Ticket> findByState(State state);
 
 }
--- src/main/java/it/milestone/backoffice/ticket_platform/restcontroller/TicketRestController.java
+++ src/main/java/it/milestone/backoffice/ticket_platform/restcontroller/TicketRestController.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.milestone.backoffice.ticket_platform.model.State;
 import it.milestone.backoffice.ticket_platform.model.Ticket;
 import it.milestone.backoffice.ticket_platform.repository.TicketRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -34,6 +35,28 @@ public class TicketRestController {
             } else {
                 return ResponseEntity.ok(ticketRepo.findAll());
             }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+    // Filtrare i ticket per categoria
+    @GetMapping("/category/{name}")
+    public ResponseEntity<List<Ticket>> getTicketsByCategory(@PathVariable String name) {
+
+        try {
+            return ResponseEntity.ok(ticketRepo.findByCategory_Name(name));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    // Filtrare i ticket per stato
+    @GetMapping("/state/{state}")
+    public ResponseEntity<List<Ticket>> getTicketsByState(@PathVariable State state) {
+        try {
+            return ResponseEntity.ok(ticketRepo.findByState(state));
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
